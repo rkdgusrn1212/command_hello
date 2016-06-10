4ef8b981f350620d5e4ddf1e5542d50875c725c4
hyeongu kang <khgkjg12@naver.com>
2016-06-10 11:26:19 +0900

    I nailed it

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello!");
 		System.out.println("long time no see");
-	}
+		System.out.println("have you?");
+	}	
 }
 		
